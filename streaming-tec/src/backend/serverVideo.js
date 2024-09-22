b1d841fbbc4ccf62ac1c1f33259a1e29f5fcfb8d
Karina <kurbinacr@gmail.com>
2024-09-21 21:11:29 -0600

    Database queries are added to the database from the server

--- streaming-tec/src/backend/serverVideo.js
+++ streaming-tec/src/backend/serverVideo.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const {saveDataVideo} = require('./fireBase/SaveDataBucket');
+const {collection, getDocs, where,query} = require('firebase/firestore');
+const {db}=require('./fireBase/fireBaseCredenciales/CredencialesServeFire');
+
 
 require('dotenv').config();
 const { Storage } = require('@google-cloud/storage');
@@ -11,35 +14,9 @@ const storage = new Storage({
 });
 
 const bucketName = 'streamingtec-video';
-const bucket = storage.bucket(bucketName);
+
 
 app.use(cors());
-
-app.get('/search/:query', async (req, res) => {
-    const query = req.params.query;
-    console.log(query)
-    try {
-        const [files] = await bucket.getFiles({
-            prefix: query,
-        });
-        
-        if (files.length === 0) {
-            return res.status(404).send('No se encontraron archivos');
-        }
-
-        const results = files.map(file => {
-            return {
-                title: file.name,
-                url: `https://storage.googleapis.com/${bucketName}/${file.name}`
-            }
-        });
-
-        res.send(results);
-
-    } catch (error) {
-        console.log("Error al encontrar el archivo: ", error);
-    }
-})
 
 //Obtener todo los datos de los videos y enviarlos a firebase
 async function getDataVideo() {
@@ -58,6 +35,35 @@ async function getProcessVideo(){
     await saveDataVideo(saveData);
 }
 getProcessVideo();
+
+
+//Consulta a Firabase
+app.get('/search/:query', async (req, res) => {
+    const searchQuery  = req.params.query;
+
+    console.log("SE ESTA BUSCANDO  = "+ searchQuery);
+    try {
+        const q =  query(collection(db, "streamingtec-video"), where('nombre', '>=', searchQuery), where('nombre', '<=', searchQuery + '\uf8ff'));
+        const snapshot = await getDocs(q)
+        if (snapshot.empty) {
+            return res.status(404).send('No se encontraron archivos');
+        }
+
+        const results = snapshot.docs.map(doc => {
+            const data = doc.data();
+            return {
+                title: data.nombre,
+                url: data.url
+            };
+        });
+        
+        res.send(results);
+
+    } catch (error) {
+        console.log("Error al consultar Firebase: ", error);
+        res.status(500).send('Error al consultar Firebase');
+    }
+});
 
 app.listen(5000, () => {
     console.log('Server is running on http://localhost:5000');

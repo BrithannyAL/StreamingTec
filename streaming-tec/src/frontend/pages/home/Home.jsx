ee9f5214752aa9aa2061968fa09d7d1b59cff64b
Brithanny Arguello <49217000+BrithannyAL@users.noreply.github.com>
2024-09-23 14:54:11 -0600

    arreglado la url de series

--- streaming-tec/src/frontend/pages/home/Home.jsx
+++ streaming-tec/src/frontend/pages/home/Home.jsx
@@ -40,10 +40,15 @@ export default function Home() {
         try {
             const response = await fetch('http://localhost:5001/random-series');
             const data = await response.json();
-            setItems(data);
+            const modifiedData = data.map(item => ({
+                ...item,
+                title: item.title.replace(/\//g, '-')
+            }));
+            setItems(modifiedData);
         } catch (error) {
             console.error("Error al obtener videos de series aleatorias: ", error);
         }
+        //AQUÍ SE LA FUNCIÓN PARA MODIFICAR LOS NOMBRES DE LAS SERIES
     };
 
     const fetchRandomVideos = async () => {
